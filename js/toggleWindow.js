c4a00c3eb80ee20f56eb1a6ef4251cca342e2400
ConorVanek <conorvanek@gmail.com>
2022-08-16 13:16:10 -0500

    Delete js directory

--- js/toggleWindow.js
+++
@@ -1,8 +0,0 @@
-function toggle() {
-  var x = document.getElementById("chat");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
